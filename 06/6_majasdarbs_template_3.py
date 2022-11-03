c6685b7fa12aa110a6b8710e9866a6879dc50d0e
Gunārs Ābeltiņš <gunars2003@gmail.com>
2022-11-03 12:44:57 +0200

    Complete 3rd task

--- 06/6_majasdarbs_template_3.py
+++ 06/6_majasdarbs_template_3.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 '''
 Python 6 nodarbības mājasdarbs Nr.3
 
@@ -9,13 +8,19 @@ import numpy as np
 import cv2
 
 # importēt "python.jpg" bildi
-img = 0 # TODO
+img = cv2.imread('python.jpg') # TODO
 
 # Pārveidot bildi melnbaltu un izvadīt uz ekrāna
-img_melnbalts = 0 # TODO
+img_melnbalts = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # TODO
+cv2.imshow('image', img_melnbalts)
+cv2.waitKey(0)
 
 # pielietot Caddy edge detection uz originālās bildes un izvadīt uz ekrāna
-img_caddy = 0 # TODO
+img_caddy = cv2.Canny(image = img, threshold1=100, threshold2=200) # TODO
+cv2.imshow('image', img_caddy)
+cv2.waitKey(0)
 
 # Pārveidot zilo krāsu par sarkanu un izvadīt uz ekrāna
-img_zils_sarkans = 0 # TODO
+img_zils_sarkans = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # TODO
+cv2.imshow('image', img_zils_sarkans)
+cv2.waitKey(0)
