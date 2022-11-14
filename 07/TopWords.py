206767f79838a7c792e57eb561c6aa978e37c1ed
Gunārs Ābeltiņš <gunars2003@gmail.com>
2022-11-14 15:23:27 +0200

    Do task

--- 07/TopWords.py
+++ 07/TopWords.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 '''
 Python 7 mājasdarbs Nr.2
 
@@ -7,6 +6,9 @@ Uzdevums: aizpildīt vietas ar atzīmi TODO
 Izveidot klasi, kura pārveido 5. nodarbības mājasdarbu Nr. 2 saturu par klasi
 
 '''
+
+import matplotlib.pyplot as plt
+import json
 
 class TopWords:
     '''
@@ -22,9 +24,18 @@ class TopWords:
     def get_bar_plot(self):
         # izsaucot šo metodi izvada bar tipa grafiku. dati ir parametrs __vardnica
         # TODO        
+        f = open('top_vardi.json')
+        data = json.load(f)
+        x = data.keys()
+        y = data.values()
+        plt.figure(figsize=(13, 5))
+        plt.title('Top vārdi')
+        plt.bar(x, y)
+        plt.show()
         return 0
 
 
 if __name__ == "__main__":
     obj = TopWords()
     obj.set_dict("top_vardi.json")
+    obj.get_bar_plot()
