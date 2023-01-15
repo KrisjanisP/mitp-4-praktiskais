992628fb4e367f8915fa9dc11cb0ccc0ef89e8f0
Gunārs Ābeltiņš <gunars2003@gmail.com>
2023-01-15 11:16:23 +0200

    Do first extra tasks

--- C++/Additional_tasks/04/main.cpp
+++ C++/Additional_tasks/04/main.cpp
@@ -19,6 +19,39 @@
 // 	4) Beigt programmu
 // Izvēlnes iespējamie gadījumi ir jādefinē kā enum tips
 
+// 1.1 -1.3. Izveidot izvēlni:
+//     1) Izveidot jaunu failu
+//     2) Parādīt jaunā faila nosaukumu
+//     3) Apvienot jauno failu ar pirmās programmas failu
+//     4) Izdzēst jauno failu
+//     5) Beigt programmu
+// - lietotājs ievada faila nosaukumu un pēc tam faila saturu
+// - attēlo uz ekrāna jaunā faila nosaukumu. Ja nav jaunā faila, tad paziņo to lietotājam
+// - jaunā faila saturu pievieno pirmās programmas failam. Ja nav jaunā faila, tad paziņo to lietotājam
+// - izdzēš jauno failu. Ja nav jaunā faila, tad paziņo to lietotājam
+// - pirms programmas beigšanas pārbauda vai gadījumā neeksistē kāds no jaunajiem failiem. Ja ir - tad paziņo lietotājam un piedāvā tos izdzēst. Ja lietotājs atsakās, tad programmu nevar pabeigt.
+// * Jaunie faili - faili, kuri tika izveidoti programmas darbības laikā
+// 2.1. Atrast summu visiem skaitļiem, kas dalās ar 3 un 5. Rezultātu ierakstīt failā file2_1.txt
+// 2.2. Nolasīt visu programmu failus, iegūto saturu saglabāt string vai char[] vai char* mainīgajā (bez atstarpēm) - tādejādi izveidosies liels skaitlis
+// 2.3. Izdalīt izveidoto skaitli ar 2 - rezultātu saglabāt failā file2_result.txt
+// 3.1. Saskaita cik ir ciparu un atstarpju - izvada uz ekrāna rezultātus
+// 3.2. Saskaita cik vārdu ir failā - izvada uz ekrāna rezultātus
+// 3.3. Saskaita cik rindu ir failā - izvada uz ekrāna rezultātus
+// 4.1. Lietotājs ievada divu simbolu virkni - saglabā saturu bez šiem burtiem file5.txt failā
+// 4.2. Lietotājs ievada vārdu - saglabā saturu bez šī vārda file5.txt failā
+// 4.3. Lietotājs ievada 2 burtus - saglabā saturu, kurā 1ais burts nomainīts pret otro burtu, file5.txt failā
+// 5.1. - 5.3.
+// Izveidot datu struktūru MiTalent, kurā ir sekojoši elementi: name, surname, age, hobby
+// Izveidot izvēlni:
+// 1) Attēlot talantus
+// 2) Pievienot talantu
+// 3) Izdzēst talantu
+// 4) Beigt darbību
+// - attēlo uz ekrāna failā visus pieejamos talantus (katru savā rindā)
+// - Lietotājs pievieno jaunu, azpildot visu informāciju par talantu, dati tiek saglabāti failā
+// - Izdzēš no faila lietotāja izvēlēto talantu (lietotājs ievada vārdu - ja eksistē, tad izdzēš, ja nav - tad paziņo lietotājam)
+// - beidz programmas darbību
+
 #include <iostream>
 #include <fstream>
 #include <limits>
@@ -116,6 +149,98 @@ void first()
     fileOut.close();
 
     cout << "Text saved to [first.txt]!" << endl;
+
+    cout << "1: Make a new file" << endl
+         << "2: Show name of new file" << endl
+         << "3. Add new file to original file" << endl
+         << "4. Remove new file" << endl
+         << "5. End program" << endl;
+
+    string fileName = "";
+    bool ok = true;
+    do
+    {
+        const eNumber choice = static_cast<eNumber>((int)get_number("[1] Choice: "));
+        switch (choice)
+        {
+        case one:
+        {
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            cout << "Enter file name: ";
+            getline(cin, fileName);
+
+            cout << "Enter file content: ";
+            string temp;
+            getline(cin, temp);
+
+            ofstream fileOut(fileName, ios::out);
+            fileOut << temp;
+
+            fileOut.close();
+
+            break;
+        }
+        case two:
+            if (fileName.c_str())
+                cout << "New file name: " << fileName << endl;
+            else
+                cout << "No new file created!" << endl;
+
+            break;
+        case three:
+        {
+            if (fileName.c_str())
+            {
+                ofstream fileOut("first.txt", ios::out | ios::app);
+                ifstream fileIn(fileName, ios::in);
+
+                while (true)
+                {
+                    string temp;
+                    getline(fileIn, temp);
+                    fileOut << '\n' << temp;
+
+                    if (fileIn.eof())
+                        break;
+                }
+                cout << "Content appended!" << endl;
+            }
+            else
+            {
+                cout << "No new file created!" << endl;
+            }
+            break;
+        }
+        case four:
+        {
+            if (fileName.c_str())
+            {
+                remove(fileName.c_str());
+                fileName = "";
+
+                cout << "File deleted!" << endl;
+            }
+            else
+            {
+                cout << "No new file created!" << endl;
+            }
+
+            break;
+        }
+        default:
+            if (!fileName.empty())
+            {
+                cout << "Can't end program without deleting file!" << endl;
+            }
+            else
+            {
+                ok = false;
+            }
+
+            break;
+        }
+    } while (ok);
 }
 
 bool isPrime(const int &x)
@@ -227,15 +352,16 @@ void fifth()
             break;
         }
         case three:
+        {
+            ifstream fileIn("fifth.bin", ios::in | ios::binary);
+            if (fileIn.is_open())
             {
-                ifstream fileIn("fifth.bin", ios::in | ios::binary);
-                if(fileIn.is_open()){
-                    fileIn.read(&date[0], date.size());
-                }
-                fileIn.close();
-                cout << "Date: " << date << endl;
-                break;
+                fileIn.read(&date[0], date.size());
             }
+            fileIn.close();
+            cout << "Date: " << date << endl;
+            break;
+        }
         default:
             ok = false;
             break;
